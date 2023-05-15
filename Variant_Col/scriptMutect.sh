05231e2150242d2cf2439d2071806a16b40c82a8
Mallory56 <128157909+Mallory56@users.noreply.github.com>
2023-05-15 13:58:53 +0200

    Update scriptMutect.sh

--- Variant_Col/scriptMutect.sh
+++ Variant_Col/scriptMutect.sh
@@ -5,23 +5,14 @@ GATK = "/gatk/gatk-4.2.0.0/gatk"
 
 FichierBam = "/data/input.bam"
 
-fichierFastaReference = "/Users/mallorylecorre/Desktop/ProjetProg/REF.fasta"
+fichierFastaReference = "/data/REF.fasta"
 
-# Chemin de sortie pour le fichier VCF
 outputFichierVcf = "/data/output.vcf"
 
-cmd = f"{GATK} HaplotypeCaller " \
+cmd = f"{GATK} Mutect2 " \
       f"-R {fichierFastaReference} " \
+      "--mitochondria " \
       f"-I {FichierBam} " \
-      f"-O {outputFichierVcf} " \
-      "--emit-ref-confidence GVCF " \
-      "--stand-call-conf 20 " \
-      "--read-filter MappingQualityReadFilter " \
-      "--filter-mapping-quality 30 " \
-      "--filter-expression \"QUAL < 30.0\" " \
-      "--filter-name \"LowQual\" " \
-      "--dbsnp /path/to/dbSNP.vcf " \
-      "--mitochondria-mode " \
-      "--interval-padding 100"
+      f"-O {outputFichierVcf}.gz"
 
 os.system(cmd)
